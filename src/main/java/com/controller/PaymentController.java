3a4fde03994342971d59c351bec4998bf51a6399
Mayur2940 <mayurwaghmare0408@gmail.com>
2024-03-19 10:47:16 +0530

    Payment Commit

--- src/main/java/com/controller/PaymentController.java
+++ src/main/java/com/controller/PaymentController.java
@@ -1,5 +1,32 @@
 package com.controller;
 
-public class PaymentController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.dto.PaymentDTO;
+import com.service.PaymentService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+	 private final PaymentService paymentService;
+ 
+	    @Autowired
+	  // PaymentServiceImpl paymentServiceImpl;
+	    public PaymentController(PaymentService paymentService) {
+	       this.paymentService = paymentService;
+	  }
+ 
+	    @PostMapping("/make")
+	    public ResponseEntity<PaymentDTO> makePayment(@RequestBody PaymentDTO paymentDTO) {
+	        PaymentDTO responsePayment = paymentService.makePayment(paymentDTO);
+	        return new ResponseEntity<>(responsePayment, HttpStatus.CREATED);
+	    }
+ 
 }
--- src/main/java/com/dto/PaymentDTO.java
+++ src/main/java/com/dto/PaymentDTO.java
@@ -5,13 +5,13 @@ import java.time.LocalDateTime;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PaymentDTO {
